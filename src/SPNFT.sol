18c5a706f9913f2d2629c1d4127fb2b1731bb2f6
devtestingstuff <nolanjacobson@protonmail.com>
2023-11-30 19:07:22 -0500

    added tokenURI functionality

--- src/SPNFT.sol
+++ src/SPNFT.sol
@@ -10,6 +10,16 @@ contract SPNFT is ERC721, Ownable {
     using Counters for Counters.Counter;
     using Strings for uint256;
 
+    struct AttributeValues {
+        string eyes;
+        string hair;
+        string nose;
+        string mouth;
+    }
+
+    // Mapping from token ID to its attributes
+    mapping(uint256 => AttributeValues) public _tokenAttributes;
+
     PostRevealNFT public revealedNFTContract;
 
     // Total Supply before reveal
@@ -21,7 +31,12 @@ contract SPNFT is ERC721, Ownable {
     string private _baseURIextended;
     string private _notRevealedURI;
 
-    constructor(string memory name, string memory symbol, uint256 _mintPrice, bool _isInCollectionReveal) ERC721(name, symbol) {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 _mintPrice,
+        bool _isInCollectionReveal
+    ) ERC721(name, symbol) {
         mintPrice = _mintPrice;
         isInCollectionReveal = _isInCollectionReveal;
     }
@@ -41,25 +56,47 @@ contract SPNFT is ERC721, Ownable {
         _baseURIextended = baseURI_;
     }
 
-    function setNotRevealedURI(string memory notRevealedURI) external onlyOwner {
+    function setNotRevealedURI(
+        string memory notRevealedURI
+    ) external onlyOwner {
         _notRevealedURI = notRevealedURI;
     }
 
-    function setRevealedNFTAddress(address _revealedNFTAddress) external onlyOwner {
+    function setRevealedNFTAddress(
+        address _revealedNFTAddress
+    ) external onlyOwner {
         revealedNFTContract = RevealedNFT(_revealedNFTAddress);
     }
 
-    function revealAndTransfer(uint256 tokenId, address user) external onlyOwner {
-        require(isInCollectionReveal == false, "This function does not work as the collection is InRevealCollection");
-        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+    function revealAndTransfer(
+        uint256 tokenId,
+        address user
+    ) external onlyOwner {
+        require(
+            isInCollectionReveal == false,
+            "This function does not work as the collection is InRevealCollection"
+        );
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
         _burn(tokenId);
         revealedNFTContract.mint(user);
     }
 
     function reveal(uint256 tokenId) public onlyOwner {
-        require(isInCollectionReveal == true, "This function does not work as the collection is not InRevealCollection");
-        require(currentSupply == MAX_TOTAL_SUPPLY, "Max supply hasn't been sold yet.");
-        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+        require(
+            isInCollectionReveal == true,
+            "This function does not work as the collection is not InRevealCollection"
+        );
+        require(
+            currentSupply == MAX_TOTAL_SUPPLY,
+            "Max supply hasn't been sold yet."
+        );
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
         revealed[tokenId] = true;
     }
 
@@ -67,14 +104,55 @@ contract SPNFT is ERC721, Ownable {
         return _baseURIextended;
     }
 
-    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+    function setTokenAttributes(
+        uint256 tokenId,
+        string memory eyes,
+        string memory hair,
+        string memory nose,
+        string memory mouth
+    ) public {
+        // Add access control as needed
+        _tokenAttributes[tokenId] = AttributeValues(eyes, hair, nose, mouth);
+    }
 
-        if(!revealed[tokenId]) {
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+
+        if (!revealed[tokenId]) {
             return _notRevealedURI;
         }
 
-        string memory baseURI = _baseURI();
-        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString())) : "";
+        AttributeValues memory attributes = _tokenAttributes[tokenId];
+        string memory json = Base64.encode(
+            bytes(
+                abi.encodePacked(
+                    '{"name": "SP',
+                    tokenId.toString(),
+                    '",',
+                    '"description": "Story Protocol NFT",',
+                    '"attributes": [',
+                    '{"trait_type": "Eyes", "value": "',
+                    attributes.eyes,
+                    '"},',
+                    '{"trait_type": "Hair", "value": "',
+                    attributes.hair,
+                    '"},',
+                    '{"trait_type": "Nose", "value": "',
+                    attributes.nose,
+                    '"},',
+                    '{"trait_type": "Mouth", "value": "',
+                    attributes.mouth,
+                    '"}',
+                    "]}"
+                )
+            )
+        );
+
+        return string(abi.encodePacked("data:application/json;base64,", json));
     }
 }
